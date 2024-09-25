ab926a7ed61753f50618da844eee68279bcc61dd
Eduardo Orestes <eduardoobrum@gmail.com>
2024-09-24 21:50:47 -0300

    First commit

---
+++ react-fundamentos/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1 id="title">
+    <span>
+      Olá mundo.
+    </span>
+  </h1>,
+  document.getElementById('root')
+);
