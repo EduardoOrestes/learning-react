2cc9bdf5f8360cb7e1e9b920ce20556ae65ff99d
Eduardo Orestes <eduardoobrum@gmail.com>
2024-09-27 22:32:11 -0300

    Props

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
 
+import Post from './Post';
+
 const category = 'Posts da semana';
+const title = 'Título da notícia';
 
 export default function App() {
   return (
     <>
-      <h1>JStack`s Blog {console.log('Log dentro do JSX')}</h1>
+      {/* added id 'idTitle' to tag */}
+      <h1 id="idTitle">JStack`s Blog {console.log('Log dentro do JSX')}</h1>
       <h2>{category}</h2>
+
+      <hr />
+
+      {/* <Post
+        title={`${title} 01`}
+        subtitle="Subtitulo da noticia 01"
+      />
+
+      <Post
+        title={`${title} 02`}
+        subtitle="Subtitulo da noticia 02"
+      />
+
+      No post receberia a props e seria acessada - { props.title }
+      */}
+
+      <Post
+        post={{
+          title: `${title} 01`,
+          subtitle: "Subtitulo da noticia 01"
+        }}
+      />
+
+      <Post
+        post={{
+          title: `${title} 02`,
+          subtitle: "Subtitulo da noticia 02"
+        }}
+      />
+
+      <Post
+        post={{
+          title: `${title} 03`,
+          subtitle: "Subtitulo da noticia 03"
+        }}
+      />
     </>
   )
 }
-
 
 // o export default pode ser no final antes da function
 // export default App;
---
+++ react-fundamentos/src/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Post(props) {
+  console.log(props);
+
+  return (
+    <>
+      <article>
+        <strong>{ props.post.title }</strong><br />
+        <small>{ props.post.subtitle }</small>
+      </article>
+
+      <br />
+    </>
+  )
+}
